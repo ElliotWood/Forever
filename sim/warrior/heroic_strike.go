83635aed72a4fd4e7e3b28aded22765e81adab2c
Adrian Klingen <adrian.klingen@deptagency.com>
2026-09-22 17:03:26 +0200

    [Warrior] Heroic Strike and Cleave each get their own file

--- sim/warrior/heroic_strike.go
+++ sim/warrior/heroic_strike.go
@@ -8,9 +8,6 @@ import (
 var heroicStrikeRank = spellData.HeroicStrike.Highest()
 var heroicStrikeBaseDamage = heroicStrikeRank.DamageEffect().Average(core.CharacterLevel)
 
-var cleaveRank = spellData.Cleave.Highest()
-var cleaveBaseDamage = cleaveRank.DamageEffect().Average(core.CharacterLevel)
-
 func (warrior *Warrior) registerHeroicStrike() {
 	config := spelldata.SpellConfig(&warrior.Unit, heroicStrikeRank, spelldata.Melee(core.ProcMaskMeleeMHSpecial))
 	// TODO: Ingame research needed if this adds flat threat
@@ -21,25 +18,6 @@ func (warrior *Warrior) registerHeroicStrike() {
 		result := spell.CalcAndDealDamage(sim, target, baseDamage, spell.OutcomeMeleeWeaponSpecialHitAndCrit)
 
 		if !result.Landed() {
-			spell.IssueRefund(sim)
-		}
-	}
-
-	warrior.RegisterSpell(config)
-}
-
-func (warrior *Warrior) registerCleave() {
-	const maxTargets int32 = 2
-
-	config := spelldata.SpellConfig(&warrior.Unit, cleaveRank, spelldata.Melee(core.ProcMaskMeleeMHSpecial))
-	// TODO: Ingame research needed if this adds flat threat
-	config.FlatThreatBonus = 0
-
-	config.ApplyEffects = func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
-		baseDamage := cleaveBaseDamage + warrior.MHWeaponDamage(sim, spell.MeleeAttackPower(target))
-		results := spell.CalcCleaveDamage(sim, target, maxTargets, baseDamage, spell.OutcomeMeleeWeaponSpecialHitAndCrit)
-		spell.DealBatchedAoeDamage(sim)
-		if !results[0].Landed() {
 			spell.IssueRefund(sim)
 		}
 	}
